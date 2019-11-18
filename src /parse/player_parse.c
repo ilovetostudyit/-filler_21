9dab7083e129f068f8edb235178d8f10d2d23f19
Ezzelyno Haggon <ehaggon@ox-q5.21-school.ru>
2019-11-18 18:00:33 +0300

    wow

--- src /parse/player_parse.c
+++ src /parse/player_parse.c
@@ -3,8 +3,7 @@
 void ft_player_parse(t_game_info *filler)
 {
     int ret;
-
-    filler->line = malloc(sizeof(char) * 500);
+    
     ret = get_next_line(STDIN_FILENO, &(filler->line));
     printf("%s\n", filler->line);
 }
